abc1c052606da16300293f708ef3b0cf21e14194
elnataoliveira <elnataoliveira@gmail.com>
2021-06-13 07:45:38 -0300

    update configure chart

--- components/ChartLine.vue
+++ components/ChartLine.vue
@@ -13,16 +13,14 @@ export default {
 
   data(){
 
-
       return {
           chartData: {
-            axis: 'y',
             labels: ['00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24'],
             datasets: [
                 {
                     label: xlabel,
                     data: data_set,
-                    backgroundColor: "rgba(54,73,93,.3)",
+                    backgroundColor: "rgba(54,26,193,.3)",
                     borderColor: "#36495d",
                     borderWidth: 1,
                     tension: 0.1
@@ -55,13 +53,17 @@ export default {
                     bottom: 10
                 }
             },
-            indexAxis: 'y',
+            responsive: true,
+            lineTension: 1,
             scales: {
-              y: {
-                  stacked: false,
-                  beginAtZero: true
-              }
-          }
+               x: {
+                grid: {
+                  borderColor: 'red'
+                }
+              },
+              
+            }
+            
         },
         ylabel: 'old'
         
@@ -70,16 +72,15 @@ export default {
     
   async mounted() {
   
-    await new Chart(this.$refs.myChart, {    
-      
+    await new Chart(this.$refs.myChart, {       
       type: 'line',
       data: this.chartData,
       options: this.options
-
     });
+    
     //this.chartData = {...this.chartData, ...{}}
-
   },
+
   computed:{
     getLabel:{
       get(){
@@ -100,52 +101,36 @@ export default {
 
     async getMessages(){
       const ylabel = []
-    //await api.get('/social-links.json').then(response => {
      await api.get('/doge.json').then(response => {
         xlabel.push(response.data.map(({ label }) => label))
-        this.ylabel = response.data.map(({ data }) => data)
-        data_set.push(response.data.map(({ data }) => data)[0])
-        //data_set.push([0.2011, 0.2013, 0.2015, 0.2014, 0.2013, 0.2011, 0.2014, 0.2013, 0.2015, 0.2013, 0.2012, 0.2011, 0.2012, 0.2013, 0.2012, 0.2012, 0.2014, 0.2012, 0.2011, 0.2013, 0.2014, 0.2016, 0.2012, 0.2015, 0.2014])
-        //data_set = [0.2011, 0.2013, 0.2015, 0.2014, 0.2013, 0.2011, 0.2014, 0.2013, 0.2015, 0.2013, 0.2012, 0.2011, 0.2012, 0.2013, 0.2012, 0.2012, 0.2014, 0.2012, 0.2011, 0.2013, 0.2014, 0.2016, 0.2012, 0.2015, 0.2014]
-        //console.log(data_set)          
+        this.ylabel = response.data.map(({ data }) => data)[0]
+        //data_set.push(response.data.map(({ data }) => data)[0])
       }).catch(e => {
           this.errors.push(e)
       });
-      alert(data_set)
-        //this.$refs.myChart.update()
+      for(var i=0;i<this.ylabel.length;i++){
+        //alert(this.ylabel.length)
+        data_set.push(this.ylabel[i]);
+      }
+      alert(this.ylabel)
+      this.$refs.myChart.update()
+      return {data_set}
     },
-
-    async setOptions(){
-      //const dados = []
-      await api.get('/doge.json').then(response => {
-        this.ylabel = response.data.map(({ data }) => data)
-        data_set.push(this.ylabel[0])
-        //this.ylabel = this.dados
-        //alert(data_set)
-        //return {dados}
-        //console.log(data_set)          
-      }).catch(e => {
-          this.errors.push(e)
-      });
-      //this.ylabel = [0.2012, 0.2015, 0.2016, 0.2013, 0.2011, 0.2012, 0.2015, 0.2014, 0.2016, 0.2014, 0.2011, 0.2013, 0.2014, 0.2011, 0.2013, 0.2014, 0.2012, 0.2011, 0.2013, 0.2012, 0.2015, 0.2016, 0.2013, 0.2014, 0.2015]
-      //alert(this.ylabel)
-      return data_set
-    },
-
-    async dogeCoin(){
+   
+    /*async dogeCoin(){
       await api.get('/doge.json').then(response => {
         this.ylabel = response.data.map(({ data }) => data)[0]
       }).catch(e => {
           this.errors.push(e)
       });
       for(var i=0;i<this.ylabel.length;i++){
-        //alert(this.ylabel[i])
+        alert(this.ylabel[i])
         data_set.push(this.ylabel[i]);
       }
       //this.$refs.myChart.update()
       //alert(`${this.ylabel}`)      
       //return `${this.ylabel}`;
-    }
+    }*/
 
    
 
